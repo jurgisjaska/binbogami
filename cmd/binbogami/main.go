6cb8bdc33488a75e1cc7f0181a171cec3549a5b2
Jurgis Jaška <jurgis.jaska@gmail.com>
2023-12-09 16:32:36 +0200

    Adds custom error handler to transform echo error to needed format

--- cmd/binbogami/main.go
+++ cmd/binbogami/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"net/http"
 
 	"github.com/jurgisjaska/binbogami/app"
+	"github.com/jurgisjaska/binbogami/app/api"
 	"github.com/jurgisjaska/binbogami/app/api/token"
 	"github.com/jurgisjaska/binbogami/app/handler"
 	"github.com/jurgisjaska/binbogami/app/handler/v1"
@@ -27,6 +30,7 @@ func main() {
 	}()
 
 	e := echo.New()
+	e.HTTPErrorHandler = customHTTPErrorHandler
 	handler.CreateAuth(e, database, config)
 
 	g := e.Group("/v1")
@@ -38,4 +42,42 @@ func main() {
 	v1.CreateCategory(g, database)
 
 	e.Logger.Fatal(e.Start(":8001"))
+}
+
+func customHTTPErrorHandler(err error, c echo.Context) {
+	if c.Response().Committed {
+		return
+	}
+
+	he, ok := err.(*echo.HTTPError)
+	if ok {
+		if he.Internal != nil {
+			if herr, ok := he.Internal.(*echo.HTTPError); ok {
+				he = herr
+			}
+		}
+	} else {
+		he = &echo.HTTPError{
+			Code:    http.StatusInternalServerError,
+			Message: http.StatusText(http.StatusInternalServerError),
+		}
+	}
+
+	code := he.Code
+	message := he.Message.(string)
+
+	if err == sql.ErrNoRows {
+		code = http.StatusNotFound
+		message = http.StatusText(code)
+	}
+
+	if c.Request().Method == http.MethodHead {
+		err = c.NoContent(he.Code)
+	} else {
+		err = c.JSON(code, api.Error(message))
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
