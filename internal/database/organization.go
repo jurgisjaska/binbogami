8cdcea33a2444b3daa9424f6fd309e3138cb425a
Jurgis Jaška <jurgis.jaska@gmail.com>
2024-01-06 12:47:01 +0200

    Adds author of the organization to the organization on organization creation

--- internal/database/organization.go
+++ internal/database/organization.go
@@ -15,7 +15,7 @@ type (
 		CreatedBy   *uuid.UUID `db:"created_by_user_id" json:"created_by"`
 		OwnedBy     *uuid.UUID `db:"owned_by_user_id" json:"owned_by"`
 
-		Members *Users `json:"members"`
+		Members []*uuid.UUID `json:"members"`
 
 		CreatedAt time.Time  `db:"created_at" json:"created_at"`
 		UpdatedAt *time.Time `db:"updated_at" json:"updated_at"`
@@ -23,12 +23,12 @@ type (
 	}
 
 	OrganizationUser struct {
-		Id             int       `json:"id"`
-		OrganizationId uuid.UUID `db:"organization_id" json:"organization_id"`
-		UserId         uuid.UUID `db:"user_id" json:"user_id"`
+		// @todo need to figure out how to manage uuids better
+		Id             int        `json:"id"`
+		OrganizationId *uuid.UUID `db:"organization_id" json:"organization_id"`
+		UserId         *uuid.UUID `db:"user_id" json:"user_id"`
 
 		CreatedAt time.Time  `db:"created_at" json:"created_at"`
-		UpdatedAt *time.Time `db:"updated_at" json:"updated_at"`
 		DeletedAt *time.Time `db:"deleted_at" json:"deleted_at"`
 	}
 
@@ -66,8 +66,20 @@ func (r *OrganizationRepository) Create(organization *Organization) error {
 		VALUES (:id, :name, :description, :created_by_user_id, :owned_by_user_id, :created_at, :updated_at, :deleted_at)
 	`, organization)
 
-	// @todo map organizations to the users
-	// when a new organization is created also should include the owner in the organization
+	if err != nil {
+		return err
+	}
+
+	// @todo what happens if this fails?
+	ou := &OrganizationUser{
+		OrganizationId: organization.Id,
+		UserId:         organization.CreatedBy,
+		CreatedAt:      time.Now(),
+	}
+	_, err = r.database.NamedExec(`
+		INSERT INTO organizations_users (id, organization_id, user_id, created_at)
+		VALUES (NULL, :organization_id, :user_id, :created_at)
+	`, ou)
 
 	if err != nil {
 		return err
