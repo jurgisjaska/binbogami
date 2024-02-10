a9c4b583d9e7dbda8fc852dfd21a21e1e5e0741a
Jurgis Jaška <jurgis.jaska@gmail.com>
2024-02-10 14:29:55 +0200

    Adds organization membership checks to the entry creation

--- internal/handler/v1/entry.go
+++ internal/handler/v1/entry.go
@@ -6,7 +6,6 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/jurgisjaska/binbogami/internal/api"
 	"github.com/jurgisjaska/binbogami/internal/api/model"
-	"github.com/jurgisjaska/binbogami/internal/api/token"
 	"github.com/jurgisjaska/binbogami/internal/database"
 	"github.com/labstack/echo/v4"
 )
@@ -35,6 +34,11 @@ func (h *Entry) initialize() *Entry {
 }
 
 func (h *Entry) create(c echo.Context) error {
+	member, err := membership(h.member, c)
+	if err != nil {
+		return c.JSON(http.StatusForbidden, api.Error(err.Error()))
+	}
+
 	entry := &model.Entry{}
 	if err := c.Bind(entry); err != nil {
 		return c.JSON(http.StatusBadRequest, api.Errors("incorrect entry data", err.Error()))
@@ -44,43 +48,31 @@ func (h *Entry) create(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, api.Errors("incorrect entry data", err.Error()))
 	}
 
-	claims := token.FromContext(c)
-	if claims.Id == nil {
-		return c.JSON(http.StatusBadRequest, api.Error("invalid authentication token"))
-	}
-
 	book, err := h.book.Find(entry.BookId)
-	if err != nil || book == nil {
+	if err != nil {
 		return c.JSON(http.StatusBadRequest, api.Error("invalid book for the entry"))
 	}
 	if book.ClosedAt != nil {
 		return c.JSON(http.StatusBadRequest, api.Error("book closed"))
 	}
 
-	member, err := h.member.ByBook(book, claims.Id)
-	if err != nil || member == nil {
-		return c.JSON(http.StatusForbidden, api.Error("only organization members can create entries"))
-	}
-
-	category, err := h.category.ByBook(book, entry.CategoryId)
-	if err != nil || category == nil {
+	_, err = h.category.ByBook(book, entry.CategoryId)
+	if err != nil {
 		return c.JSON(http.StatusForbidden, api.Error("category does not belong to the book"))
 	}
 
-	location, err := h.location.ByBook(book, entry.LocationId)
-	if err != nil || location == nil {
+	_, err = h.location.ByBook(book, entry.LocationId)
+	if err != nil {
 		return c.JSON(http.StatusForbidden, api.Error("location does not belong to the book"))
 	}
 
-	entry.CreatedBy = claims.Id
+	entry.CreatedBy = member.UserId
 	entity, err := h.repository.Create(entry)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, api.Error(err.Error()))
 	}
 
 	return c.JSON(http.StatusOK, api.Success(entity, api.CreateRequest(c)))
-
-	return nil
 }
 
 func CreateEntry(g *echo.Group, d *sqlx.DB) *Entry {
