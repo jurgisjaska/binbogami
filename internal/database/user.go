b09650bf5557747fc1551366992cc180f83eb43f
Jurgis Jaška <jurgis.jaska@gmail.com>
2024-03-16 14:51:25 +0200

    Updates user JSON notation to camelCase

--- internal/database/user.go
+++ internal/database/user.go
@@ -16,9 +16,9 @@ type (
 		Surname   *string    `json:"surname"`
 		Salt      string     `json:"-"`
 		Password  string     `json:"-"`
-		CreatedAt time.Time  `db:"created_at" json:"created_at"`
-		UpdatedAt *time.Time `db:"updated_at" json:"updated_at"`
-		DeletedAt *time.Time `db:"deleted_at" json:"deleted_at"`
+		CreatedAt time.Time  `db:"created_at" json:"createdAt"`
+		UpdatedAt *time.Time `db:"updated_at" json:"updatedAt"`
+		DeletedAt *time.Time `db:"deleted_at" json:"deletedAt"`
 	}
 
 	Users []User
