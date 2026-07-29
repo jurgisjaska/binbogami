33554cfc244657d526a8f519a225925de81ab703
Jurgis Jaška <jurgis.jaska@gmail.com>
2026-07-29 14:39:04 +0300

    Improves audit log messages and HTTP status codes in `forgot` and `signin` handlers, enhancing error specificity and consistency.

--- internal/handlers/auth/forgot.go
+++ internal/handlers/auth/forgot.go
@@ -16,20 +16,20 @@ const passwordResetLimit int = 10
 func (h *Auth) forgot(c *echo.Context) error {
 	request := &auth.ForgotRequest{}
 	if err := c.Bind(request); err != nil {
-		h.auditlog.Warn("forgot error", "error", err.Error())
+		h.auditlog.Warn("forgot error: bad request", "error", err.Error())
 		return c.JSON(http.StatusBadRequest, api.Error(requestError))
 	}
 
 	if err := c.Validate(request); err != nil {
-		h.auditlog.Warn("forgot error", "error", err.Error())
+		h.auditlog.Warn("forgot error: validation error", "error", err.Error())
 		return c.JSON(http.StatusUnprocessableEntity, api.Errors(validationError, err.Error()))
 	}
 
 	// attempt to locate user (active) by email
 	user, err := h.user.repository.FindActiveByEmail(request.Email)
 	if err != nil {
-		h.auditlog.Warn("forgot error", "email", request.Email, "error", err.Error())
-		return c.JSON(http.StatusUnprocessableEntity, api.Errors("no user associated with this email", err.Error()))
+		h.auditlog.Warn("forgot error: user not found", "email", request.Email, "error", err.Error())
+		return c.JSON(http.StatusNotFound, api.Errors("user not found", err.Error()))
 	}
 
 	// find other password resets for the repository
@@ -37,7 +37,7 @@ func (h *Auth) forgot(c *echo.Context) error {
 
 	// verify that repository does not have many of them
 	if resets != nil && len(*resets) >= passwordResetLimit {
-		h.auditlog.Warn("forgot error", "user_id", user.Id, "error", "too many password resets")
+		h.auditlog.Warn("forgot error: too many password resets", "user_id", user.Id)
 		return c.JSON(http.StatusUnprocessableEntity, api.Error("too many password resets"))
 	}
 
@@ -53,14 +53,14 @@ func (h *Auth) forgot(c *echo.Context) error {
 	// save new password reset
 	err = h.user.passwordReset.Create(reset)
 	if err != nil {
-		h.auditlog.Warn("forgot error", "user_id", user.Id, "error", err.Error())
+		h.auditlog.Warn("forgot error: unable to create password reset token", "user_id", user.Id, "error", err.Error())
 		return c.JSON(http.StatusInternalServerError, api.Error(err.Error()))
 	}
 
 	// send email with reset password link
 	err = h.mailer.resetPassword.Send(user, reset)
 	if err != nil {
-		h.auditlog.Warn("forgot error", "user_id", user.Id, "email", user.Email, "error", err.Error())
+		h.auditlog.Warn("forgot error: email error", "user_id", user.Id, "email", user.Email, "error", err.Error())
 		return c.JSON(http.StatusInternalServerError, api.Error(err.Error()))
 	}
 
--- internal/handlers/auth/signin.go
+++ internal/handlers/auth/signin.go
@@ -14,30 +14,31 @@ import (
 func (h *Auth) signin(c *echo.Context) error {
 	request := &auth.SigninRequest{}
 	if err := c.Bind(request); err != nil {
-		h.auditlog.Warn("signin error", "error", err.Error())
+		h.auditlog.Warn("signin error: bad request", "error", err.Error())
 		return c.JSON(http.StatusBadRequest, api.Error(credentialError))
 	}
 
 	if err := c.Validate(request); err != nil {
-		h.auditlog.Warn("signin error", "error", err.Error())
-		return c.JSON(http.StatusBadRequest, api.Errors(credentialError, err.Error()))
+		h.auditlog.Warn("signin error: validation error", "error", err.Error())
+		return c.JSON(http.StatusUnprocessableEntity, api.Errors(credentialError, err.Error()))
 	}
 
 	u, err := h.user.repository.FindActiveByEmail(request.Email)
 	if err != nil {
-		h.auditlog.Warn("signin error", "email", request.Email, "error", err.Error())
-		return c.JSON(http.StatusUnprocessableEntity, api.Errors(credentialError, err.Error()))
+		h.auditlog.Warn("signin error: user not found", "email", request.Email, "error", err.Error())
+		return c.JSON(http.StatusUnauthorized, api.Errors(credentialError, err.Error()))
 	}
 
 	password := h.buildPassword(request.Password, u.Salt)
 	if err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)); err != nil {
-		h.auditlog.Warn("signin error", "user_id", u.Id, "error", err.Error())
+		h.auditlog.Warn("signin error: incorrect password", "user_id", u.Id, "error", err.Error())
 		return c.JSON(http.StatusUnauthorized, api.Error(err.Error()))
 	}
 
 	t, err := token.CreateToken(u, h.configuration.Secret)
 	if err != nil {
-		return c.JSON(http.StatusUnauthorized, api.Error(err.Error()))
+		h.auditlog.Error("signin error: token creation error", "user_id", u.Id, "error", err.Error())
+		return c.JSON(http.StatusInternalServerError, api.Error(err.Error()))
 	}
 
 	response := auth.SigninResponse{Token: t, User: u}
