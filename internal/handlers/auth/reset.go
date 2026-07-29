a700bdb05552f34c88a8c77ebc4a5d34671f5eb1
Jurgis Jaška <jurgis.jaska@gmail.com>
2026-07-29 14:07:00 +0300

    Enhances audit logging in `reset` handler, adds validation for unopened tokens, updates password reset token expiration logic, and improves error handling and messaging.

--- internal/handlers/auth/reset.go
+++ internal/handlers/auth/reset.go
@@ -39,39 +39,51 @@ func (h *Auth) open(c *echo.Context) error {
 func (h *Auth) reset(c *echo.Context) error {
 	request := &auth.ResetPasswordRequest{}
 	if err := c.Bind(request); err != nil {
+		h.auditlog.Warn("password reset error: bad request", "error", err.Error())
 		return c.JSON(http.StatusBadRequest, api.Error(requestError))
 	}
 
 	if err := c.Validate(request); err != nil {
+		h.auditlog.Warn("password reset error: validation error", "error", err.Error())
 		return c.JSON(http.StatusUnprocessableEntity, api.Errors(validationError, err.Error()))
 	}
 
 	// retrieve the password reset token
 	entity, err := h.user.passwordReset.Find(request.Token)
 	if err != nil {
+		h.auditlog.Warn("password reset error: password reset token not found", "error", err.Error(), "token", request.Token)
 		return c.JSON(http.StatusNotFound, api.Error("password reset token not found"))
 	}
 
-	// retrieve the repository attempting to reset password
+	// check if the token is opened
+	// if token is not opened, password reset is not allowed
+	if entity.OpenedAt == nil {
+		h.auditlog.Warn("password reset error: password reset token not opened", "token", request.Token)
+		return c.JSON(http.StatusUnauthorized, api.Error("password reset token not opened"))
+	}
+
+	// retrieve the user attempting to reset password
 	user, err := h.user.repository.Find(entity.UserId)
 	if err != nil {
+		h.auditlog.Warn("password reset error: user not found", "error", err.Error(), "token", request.Token)
 		return c.JSON(http.StatusUnauthorized, api.Errors(credentialError, err.Error()))
 	}
 
 	user.Password, err = h.hashPassword(request.Password, user.Salt)
 	if err != nil {
+		h.auditlog.Warn("password reset error: password hashing failure", "error", err.Error(), "token", request.Token)
 		return c.JSON(http.StatusInternalServerError, api.Errors(internalError, err.Error()))
 	}
 
 	err = h.user.repository.UpdatePassword(user)
 	if err != nil {
+		h.auditlog.Warn("password reset error: failed to update password", "error", err.Error(), "token", request.Token)
 		return c.JSON(http.StatusInternalServerError, api.Errors(internalError, err.Error()))
 	}
 
-	err = h.user.passwordReset.UpdateExpireAt(user)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, api.Errors(internalError, err.Error()))
-	}
+	n := time.Now()
+	entity.ExpireAt = n
+	_ = h.user.passwordReset.Update(entity)
 
 	return c.JSON(http.StatusOK, api.Success(user, api.CreateRequest(c)))
 }
